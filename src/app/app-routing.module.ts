213decc017a57a9cf5ffe678a326069d9b73932b
StanislawNagorski <stanislaw.nagorski@gmail.com>
2021-01-03 17:38:59 +0100

    list of products component added to routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {ListOfProductsComponent} from './containers/list-of-products/list-of-products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'products',
+    component: ListOfProductsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
